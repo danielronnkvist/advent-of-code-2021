d92ad18eb862905c3a5fed5dcae334a0b205865a
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-01 09:01:23 +0100

    day1 refactor

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,42 +1,22 @@
 fn main() {
-    let result = advent_of_code::read_lines("inputs/day1.txt")
-        .unwrap()
-        .into_iter()
-        .fold((0, i32::MAX), |(count, previous), depth| {
-            let depth: i32 = depth.unwrap().parse().expect("parse line as i32");
-            if depth > previous {
-                (count + 1, depth)
-            } else {
-                (count, depth)
-            }
-        })
-        .0;
-    println!("Puzzle 1: {}", result);
-
     let lines = advent_of_code::read_lines("inputs/day1.txt")
         .unwrap()
         .into_iter()
         .map(|depth| depth.unwrap().parse::<i32>().expect("parse line as i32"))
         .collect::<Vec<i32>>();
-    let result = lines
-        .iter()
-        .enumerate()
-        .map(|(index, w1)| {
-            if index + 2 == lines.len() {
-                return None;
-            }
-            let w2 = lines.get(index + 1).unwrap();
-            let w3 = lines.get(index + 2).unwrap();
-            Some(w1 + w2 + w3)
-        })
-        .take_while(|x| x.is_some())
-        .fold((0, i32::MAX), |(count, previous), depth| {
-            if depth.unwrap() > previous {
-                (count + 1, depth.unwrap())
-            } else {
-                (count, depth.unwrap())
-            }
-        })
-        .0;
-    println!("Puzzle 2: {}", result);
+
+    let mut p1 = 0;
+    let mut p2 = 0;
+    for i in 0..lines.len() {
+        if i >= 1 && lines[i] > lines[i - 1] {
+            p1 += 1;
+        }
+        if i >= 3
+            && lines[i] + lines[i - 1] + lines[i - 2] > lines[i - 1] + lines[i - 2] + lines[i - 3]
+        {
+            p2 += 1;
+        }
+    }
+    println!("Puzzle 1: {}", p1);
+    println!("Puzzle 2: {}", p2);
 }
