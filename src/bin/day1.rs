576e26e65c965f166d44f566e0f6e846638975a7
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-01 08:49:04 +0100

    day1 puzzle 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -12,4 +12,31 @@ fn main() {
         })
         .0;
     println!("Puzzle 1: {}", result);
+
+    let lines = advent_of_code::read_lines("inputs/day1.txt")
+        .unwrap()
+        .into_iter()
+        .map(|depth| depth.unwrap().parse::<i32>().expect("parse line as i32"))
+        .collect::<Vec<i32>>();
+    let result = lines
+        .iter()
+        .enumerate()
+        .map(|(index, w1)| {
+            if index + 2 == lines.len() {
+                return None;
+            }
+            let w2 = lines.get(index + 1).unwrap();
+            let w3 = lines.get(index + 2).unwrap();
+            Some(w1 + w2 + w3)
+        })
+        .take_while(|x| x.is_some())
+        .fold((0, i32::MAX), |(count, previous), depth| {
+            if depth.unwrap() > previous {
+                (count + 1, depth.unwrap())
+            } else {
+                (count, depth.unwrap())
+            }
+        })
+        .0;
+    println!("Puzzle 2: {}", result);
 }
