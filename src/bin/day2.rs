e61c74c854c52e5ac9f2e28449808d11a68d5d5d
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-02 08:34:52 +0100

    fix(day2): one loop

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,25 +1,9 @@
 fn main() {
     let lines: Vec<String> = advent_of_code::read_and_parse("inputs/day2.txt");
 
-    let mut depth = 0;
+    let mut p1_depth = 0;
+    let mut p2_depth = 0;
     let mut horizontal = 0;
-    for c in &lines {
-        let mut splits = c.split_whitespace();
-        let command = splits.next().unwrap();
-        let value: i32 = splits.next().unwrap().parse().unwrap();
-
-        match command {
-            "forward" => horizontal += value,
-            "up" => depth -= value,
-            "down" => depth += value,
-            _ => unimplemented!(),
-        };
-    }
-    println!("Puzzle 1: {}", depth * horizontal);
-
-    let mut depth = 0;
-    let mut horizontal = 0;
-    let mut aim = 0;
     for c in &lines {
         let mut splits = c.split_whitespace();
         let command = splits.next().unwrap();
@@ -28,12 +12,13 @@ fn main() {
         match command {
             "forward" => {
                 horizontal += value;
-                depth += aim * value;
+                p2_depth += p1_depth * value;
             }
-            "up" => aim -= value,
-            "down" => aim += value,
+            "up" => p1_depth -= value,
+            "down" => p1_depth += value,
             _ => unimplemented!(),
         };
     }
-    println!("Puzzle 2: {}", depth * horizontal);
+    println!("Puzzle 1: {}", p1_depth * horizontal);
+    println!("Puzzle 2: {}", p2_depth * horizontal);
 }
