e96b8f63ce9796d0d9ab6351353fcb8e365bdff4
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-05 10:54:47 +0100

    cleanup one iteration

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -12,10 +12,15 @@ fn insert(map: &mut HashMap<Point, i32>, key: Point) {
 }
 
 fn main() {
-    let lines: Vec<Line> = advent_of_code::read_and_parse("inputs/day5.txt")
-        .iter()
-        .map(|line: &String| {
-            line.split(" -> ")
+    let mut straight_lines: HashMap<Point, i32> = HashMap::new();
+    let mut diagonal_lines: HashMap<Point, i32> = HashMap::new();
+    advent_of_code::read_lines("inputs/day5.txt")
+        .unwrap()
+        .into_iter()
+        .for_each(|line| {
+            let line = line.unwrap();
+            let line: Line = line
+                .split(" -> ")
                 .map(|p| {
                     p.split(",")
                         .map(|n| n.parse::<i32>().unwrap())
@@ -25,30 +30,25 @@ fn main() {
                 })
                 .collect::<Vec<Point>>()[0..2]
                 .try_into()
-                .unwrap()
-        })
-        .collect();
+                .unwrap();
+            let x1 = line[0][0];
+            let x2 = line[1][0];
+            let y1 = line[0][1];
+            let y2 = line[1][1];
 
-    let mut straight_lines: HashMap<Point, i32> = HashMap::new();
-    let mut diagonal_lines: HashMap<Point, i32> = HashMap::new();
-    for line in &lines {
-        let x1 = line[0][0];
-        let x2 = line[1][0];
-        let y1 = line[0][1];
-        let y2 = line[1][1];
+            let x_step = x1.cmp(&x2) as i32;
+            let y_step = y1.cmp(&y2) as i32;
+            let range_target = (x1 - x2).abs().max((y1 - y2).abs());
 
-        let x_step = x1.cmp(&x2) as i32;
-        let y_step = y1.cmp(&y2) as i32;
-        let range_target = (x1.max(x2) - x1.min(x2)).max(y1.max(y2) - y1.min(y2));
-
-        for i in 0..=range_target {
-            let key = [x1 - (x_step * i), y1 - (y_step * i)];
-            insert(&mut diagonal_lines, key);
-            if x_step == 0 || y_step == 0 {
-                insert(&mut straight_lines, key);
+            for i in 0..=range_target {
+                let key = [x1 - (x_step * i), y1 - (y_step * i)];
+                insert(&mut diagonal_lines, key);
+                if x_step == 0 || y_step == 0 {
+                    insert(&mut straight_lines, key);
+                }
             }
-        }
-    }
+        });
+
     let sum = straight_lines.values().filter(|&&v| v > 1).count();
     println!("Puzzle 1: {}", sum);
 
