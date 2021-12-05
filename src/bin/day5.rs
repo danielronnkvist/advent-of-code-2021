50d8a6b26d14d7261d48a17b3f8f85659fba7e7e
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-05 10:18:28 +0100

    day5 puzzle 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -3,6 +3,14 @@ use std::collections::HashMap;
 type Point = [i32; 2];
 type Line = [Point; 2];
 
+fn insert(map: &mut HashMap<Point, i32>, key: Point) {
+    if let Some(p) = map.get_mut(&key) {
+        *p += 1;
+    } else {
+        map.insert(key, 1);
+    }
+}
+
 fn main() {
     let lines: Vec<Line> = advent_of_code::read_and_parse("inputs/day5.txt")
         .iter()
@@ -21,32 +29,44 @@ fn main() {
         })
         .collect();
 
-    let mut targets: HashMap<Point, i32> = HashMap::new();
+    let mut straight_lines: HashMap<Point, i32> = HashMap::new();
+    let mut diagonal_lines: HashMap<Point, i32> = HashMap::new();
     for line in &lines {
+        let x1 = line[0][0];
+        let x2 = line[1][0];
+        let y1 = line[0][1];
+        let y2 = line[1][1];
         if line[0][0] == line[1][0] {
-            let y1 = line[0][1];
-            let y2 = line[1][1];
             for y in y1.min(y2)..=y1.max(y2) {
                 let key = [line[0][0], y];
-                if let Some(p) = targets.get_mut(&key) {
-                    *p += 1;
-                } else {
-                    targets.insert(key, 1);
-                }
+                insert(&mut straight_lines, key);
+                insert(&mut diagonal_lines, key);
             }
         } else if line[0][1] == line[1][1] {
-            let x1 = line[0][0];
-            let x2 = line[1][0];
             for x in x1.min(x2)..=x1.max(x2) {
                 let key = [x, line[0][1]];
-                if let Some(p) = targets.get_mut(&key) {
-                    *p += 1;
-                } else {
-                    targets.insert(key, 1);
-                }
+                insert(&mut straight_lines, key);
+                insert(&mut diagonal_lines, key);
+            }
+        } else {
+            let x_step = match x1 < x2 {
+                true => 1,
+                false => -1,
+            };
+            let y_step = match y1 < y2 {
+                true => 1,
+                false => -1,
+            };
+
+            for i in 0..=(x1.max(x2) - x1.min(x2)) {
+                let key = [x1 + (x_step * i), y1 + (y_step * i)];
+                insert(&mut diagonal_lines, key);
             }
         }
     }
-    let sum = targets.values().filter(|&&v| v > 1).count();
+    let sum = straight_lines.values().filter(|&&v| v > 1).count();
     println!("Puzzle 1: {}", sum);
+
+    let sum = diagonal_lines.values().filter(|&&v| v > 1).count();
+    println!("Puzzle 2: {}", sum);
 }
