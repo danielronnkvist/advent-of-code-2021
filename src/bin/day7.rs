25fd640050396bb801da37f47a5282e22209996c
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-07 08:27:46 +0100

    day7 puzzle 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,12 +1,13 @@
 fn main() {
-    let mut crabs: Vec<i32> = advent_of_code::read_lines("inputs/day7.txt")
+    let mut crabs: Vec<i64> = advent_of_code::read_lines("inputs/day7.txt")
         .unwrap()
         .next()
         .unwrap()
         .unwrap()
         .split(",")
-        .map(|x| x.parse().unwrap())
+        .map(|x| x.parse::<i64>().unwrap())
         .collect();
+
     crabs.sort();
     let target = crabs[crabs.len() / 2];
 
@@ -14,4 +15,16 @@ fn main() {
         .iter()
         .fold(0, |sum, crab| sum + (crab - target).abs());
     println!("Puzzle 1: {:?}", sum);
+
+    let max = crabs.iter().max();
+    let mut counted = vec![];
+    for i in 0..*max.unwrap() {
+        let sum: i64 = crabs.iter().fold(0, |sum, crab| {
+            let n = (crab - i).abs();
+            sum + ((n * (n + 1)) / 2)
+        });
+        counted.push(sum);
+    }
+
+    println!("Puzzle 2: {:?}", counted.iter().min().unwrap());
 }
