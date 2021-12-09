6a17445273f86d43377c16eb197bc864b4e67ea5
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-09 08:27:01 +0100

    day9 puzzle 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,3 +1,32 @@
+fn two_d_get(map: &Vec<Vec<i32>>, x: i32, y: i32) -> Option<i32> {
+    if x < 0 || y < 0 || x >= map[0].len() as i32 || y >= map.len() as i32 {
+        return None;
+    }
+    if let Some(row) = map.get(y as usize) {
+        if let Some(t) = row.get(x as usize) {
+            return Some(*t);
+        }
+    }
+    None
+}
+
+fn find_basin(visited: &mut Vec<(i32, i32)>, map: &Vec<Vec<i32>>, x: i32, y: i32) -> i32 {
+    let mut result = 0;
+    let points = vec![(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)];
+    for point in points {
+        if !visited.contains(&point) {
+            if let Some(n) = two_d_get(map, point.0, point.1) {
+                if n < 9 {
+                    result += 1;
+                    visited.push(point);
+                    result += find_basin(visited, map, point.0, point.1);
+                }
+            }
+        }
+    }
+    result
+}
+
 fn is_lower(curr: i32, map: &Vec<Vec<i32>>, x: i32, y: i32) -> Option<bool> {
     if x < 0 || y < 0 || x >= map[0].len() as i32 || y >= map.len() as i32 {
         return None;
@@ -19,7 +48,9 @@ fn main() {
                 .collect()
         })
         .collect();
+
     let mut p1 = 0;
+    let mut p2 = Vec::new();
     for y in 0..heightmap.len() as i32 {
         for x in 0..heightmap[0].len() as i32 {
             let curr = heightmap[y as usize][x as usize];
@@ -34,8 +65,16 @@ fn main() {
 
             if is_lowest {
                 p1 += curr + 1;
+
+                let mut basin_locations = vec![(x, y)];
+                let basin = 1 + find_basin(&mut basin_locations, &heightmap, x, y);
+                p2.push(basin);
             }
         }
     }
     println!("Puzzle 1: {}", p1);
+    p2.sort();
+    p2.reverse();
+    let p2 = p2[0] * p2[1] * p2[2];
+    println!("Puzzle 2: {}", p2);
 }
