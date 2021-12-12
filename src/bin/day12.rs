eceb3ea70b02e7fce49213c9b1c8dc7bef618733
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-12 11:33:21 +0100

    day12 puzzle 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -28,6 +28,52 @@ fn find_paths_to_end(
     count
 }
 
+fn find_paths_to_end_p2(
+    path: &Vec<String>,
+    start: &str,
+    caves: &HashMap<String, Vec<String>>,
+) -> usize {
+    let mut count = 0;
+
+    for cave in caves.get(start).unwrap() {
+        if cave == "start" {
+            continue;
+        } else if cave == "end" {
+            count += 1;
+        } else if &cave.to_lowercase() == cave
+            && (!path
+                .iter()
+                .fold(HashMap::new(), |mut sum, c| {
+                    if &c.to_lowercase() == c {
+                        if let Some(a) = sum.get_mut(c) {
+                            *a += 1;
+                        } else {
+                            sum.insert(c, 1);
+                        }
+                    }
+                    sum
+                })
+                .values()
+                .any(|&v| v == 2)
+                || !path.contains(cave))
+        {
+            count += find_paths_to_end_p2(
+                &vec![path.clone(), vec![cave.to_string()]].concat(),
+                cave,
+                caves,
+            );
+        } else if &cave.to_uppercase() == cave {
+            count += find_paths_to_end_p2(
+                &vec![path.clone(), vec![cave.to_string()]].concat(),
+                cave,
+                caves,
+            );
+        }
+    }
+
+    count
+}
+
 fn main() {
     let mut caves: HashMap<String, Vec<String>> = HashMap::new();
     advent_of_code::read_lines("inputs/day12.txt")
@@ -51,4 +97,7 @@ fn main() {
 
     let p1 = find_paths_to_end(&vec!["start".to_string()], "start", &caves);
     println!("Puzzle 1: {}", p1);
+
+    let p2 = find_paths_to_end_p2(&vec!["start".to_string()], "start", &caves);
+    println!("Puzzle 2: {}", p2);
 }
