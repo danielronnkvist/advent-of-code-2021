828acf652b99206840d706c7b600020b045280d6
Daniel Rönnkvist <daniel@ronnkvist.com>
2021-12-13 08:31:09 +0100

    day13 puzzle 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,3 +1,29 @@
+fn fold(coordinates: &Vec<(i32, i32)>, fold: (char, i32)) -> Vec<(i32, i32)> {
+    coordinates.iter().fold(vec![], |mut sum, (x, y)| {
+        let coordinate = {
+            if fold.0 == 'x' {
+                if x > &fold.1 {
+                    let x = fold.1 - (x - fold.1);
+                    (x, *y)
+                } else {
+                    (*x, *y)
+                }
+            } else {
+                if y > &fold.1 {
+                    let y = fold.1 - (y - fold.1);
+                    (*x, y)
+                } else {
+                    (*x, *y)
+                }
+            }
+        };
+        if !sum.contains(&coordinate) {
+            sum.push(coordinate);
+        }
+        sum
+    })
+}
+
 fn main() {
     let mut coordinates: Vec<(i32, i32)> = vec![];
     let mut folds: Vec<(char, i32)> = vec![];
@@ -22,32 +48,20 @@ fn main() {
             }
         });
 
-    let fold = folds[0];
-    let p1 = coordinates
-        .iter()
-        .fold(vec![], |mut sum, (x, y)| {
-            let coordinate = {
-                if fold.0 == 'x' {
-                    if x > &fold.1 {
-                        let x = fold.1 - (x - fold.1);
-                        (x, *y)
-                    } else {
-                        (*x, *y)
-                    }
-                } else {
-                    if y > &fold.1 {
-                        let y = fold.1 - (y - fold.1);
-                        (*x, y)
-                    } else {
-                        (*x, *y)
-                    }
-                }
-            };
-            if !sum.contains(&coordinate) {
-                sum.push(coordinate);
-            }
-            sum
-        })
-        .len();
+    let p1 = fold(&coordinates, folds[0]).len();
     println!("Puzzle 1: {}", p1);
+
+    for f in folds {
+        coordinates = fold(&coordinates, f);
+    }
+    for y in 0..6 {
+        for x in 0..5 * 8 {
+            if coordinates.contains(&(x, y)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
 }
